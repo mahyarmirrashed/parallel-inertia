5fd4fe6f8444507b848b969d18d6674d7bca817d
OwenBurland <77137879+OwenBurland@users.noreply.github.com>
2022-10-15 14:09:43 -0500

    Added day checkboxes

--- client/src/Components/Habit.js
+++ client/src/Components/Habit.js
@@ -1,10 +1,31 @@
+import { Button, Checkbox, Typography, Grid } from "@mui/material" 
 
 const Habit = ({habit, onDelete}) => {
   return (
+    
     <div className={`habit`}>
-      <h3>{habit.text}</h3>
-      <h4>{habit.day} at { habit.time}</h4>
-      <p>{habit.description}</p>
+      <Typography variant="h5">{habit.text}</Typography>
+      <Typography variant="h6">
+        <Grid>
+          M:
+          <Checkbox checked = {habit.monday}></Checkbox>
+          T:
+          <Checkbox checked = {habit.tuesday}></Checkbox>
+          W:
+          <Checkbox checked = {habit.wednesday}></Checkbox>
+          R:
+          <Checkbox checked = {habit.thursday}></Checkbox>
+          F:
+          <Checkbox checked = {habit.friday}></Checkbox>
+          S:
+          <Checkbox checked = {habit.saturday}></Checkbox>
+          U:
+          <Checkbox checked = {habit.sunday}></Checkbox>
+          <Button variant="text" onClick = {onDelete} >Delete</Button>
+        </Grid>
+        </Typography>
+      <Typography>{habit.time}</Typography>
+      <Typography>{habit.description}</Typography>
     </div>
   )
 }
