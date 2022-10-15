1f88be334a9bcb70f3fc0dc2369404cba505480e
Mahyar Mirrashed <mah.mirr@gmail.com>
2022-10-15 15:08:43 -0500

    feat: update needed queries for logging in

--- src/queries.js
+++ src/queries.js
@@ -1,27 +1,36 @@
-const generateCookie = (username, password) => {
-  // find username/password combination
-  // if username does not exist, then create new user and set password, otherwise
-  // if username/password combination matches, create new cookie and return it
-  // when creating cookie, add to cookie collection and map to username
-  // if username/password combination does not exist, return null
+const { v4: uuidv4 } = require('uuid');
+
+const Cookie = require('./models/Cookie');
+const User = require('./models/User');
+
+const createCookie = async (username) => {
+  let cookie = uuidv4();
+
+  try {
+    Cookie.findOneAndUpdate(
+      { username: username },
+      { username: username, cookie: cookie },
+      { upsert: true },
+    );
+
+    return cookie;
+  } catch {
+    return null;
+  }
 };
 
-const getUsernameFromCookie = (cookie) => {
-  // search for cookie inside cookies collection
-  // if cookie is not found, return null (403 forbidden)
-  // if cookie is found, return username
-  // (optional), delete expired cookies
-};
+const createUser = async (username, password) =>
+  new User({ username: username, password: password }).save();
 
-const getHabitData = (cookie) => {
-  // call getUsernameFromCookie()
-  // get list of habits for username
-  // trim dates by sorted, and return most recent five
-};
+const userExists = async (username) =>
+  User.findOne({ username: username }).exec() !== null;
 
-const logHabit = (cookie, habitId, date, status) => {
-  // call getUsernameFromCookie()
-  // look up username/habitId combination
-  // if habitId does not exist on username, return false (400 bad request)
-  // if habitId does exist, add/remove date on habitId based on status (200 OK)
+const userIsValid = async (username, password) =>
+  User.findOne({ username: username, password: password }).exec() !== null;
+
+module.exports = {
+  createCookie,
+  createUser,
+  userExists,
+  userIsValid,
 };
