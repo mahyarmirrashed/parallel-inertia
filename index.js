c3f062579b1a6929d5bd778a3df01420af47e65d
Mahyar Mirrashed <mah.mirr@gmail.com>
2022-10-15 11:55:28 -0500

    feat: add feature to read and set cookies

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 // set up backend listening port
 const PORT = process.env.PORT || 4000;
@@ -6,7 +7,12 @@ const PORT = process.env.PORT || 4000;
 // initialize express.js backend application
 const app = express();
 
+// set up cookie parsing
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
+  console.log(req.cookies);
+  res.cookie('Session-ID', 'fff5ec44-7589-442d-9346-6dd99f9b90a6');
   res.send({ message: 'Hello from server!' });
 });
 
