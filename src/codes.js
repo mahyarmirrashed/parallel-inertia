4ccd36b0afa0b52a073f0046cf2359e94fb888da
Mahyar Mirrashed <mah.mirr@gmail.com>
2022-10-15 14:56:50 -0500

    feat: add http status codes

---
+++ src/codes.js
@@ -0,0 +1,13 @@
+const OK = 200;
+const CREATED = 201;
+const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
+const INTERNAL_SERVER_ERROR = 500;
+
+module.exports = {
+  OK,
+  CREATED,
+  BAD_REQUEST,
+  UNAUTHORIZED,
+  INTERNAL_SERVER_ERROR,
+};
